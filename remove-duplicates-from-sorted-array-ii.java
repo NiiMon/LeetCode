310a9929e7b4689ff21c0bc5533341e2de048b47
NiiMon <35044329+NiiMon@users.noreply.github.com>
2018-01-02 14:50:30 -0800

    80. Remove Duplicates from Sorted Array II

---
+++ remove-duplicates-from-sorted-array-ii.java
@@ -0,0 +1,31 @@
+/*
+80. Remove Duplicates from Sorted Array II
+https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/description/
+
+
+Follow up for "Remove Duplicates": What if duplicates are allowed at most
+twice?
+
+For example, Given sorted array nums = [1,1,1,2,2,3],
+
+Your function should return length = 5, with the first five elements of nums
+being 1, 1, 2, 2 and 3. It doesn't matter what you leave beyond the new
+length.
+
+
+*/
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int lastIndex = -1;
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (lastIndex < 1 || nums[i] != nums[lastIndex - 1]) {
+                nums[++lastIndex] = nums[i];
+            }
+        }
+        
+        return lastIndex + 1;
+    }
+}
+
