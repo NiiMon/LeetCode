f015c574929486132045085030d12a86c51e56bd
NiiMon <35044329+NiiMon@users.noreply.github.com>
2018-01-15 18:53:27 -0800

    141. Linked List Cycle
    slow & fast pointers

---
+++ linked-list-cycle.java
@@ -0,0 +1,45 @@
+/*
+
+141. Linked List Cycle
+https://leetcode.com/problems/linked-list-cycle/description/
+
+
+Given a linked list, determine if it has a cycle in it.
+
+Follow up:
+Can you solve it without using extra space?
+
+*/
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        
+        ListNode slow = head;
+        ListNode fast = head.next;
+        
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+            
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        return true;
+    }
+}
+
