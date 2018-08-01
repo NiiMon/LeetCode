5b2ef61715f867c06eda3375a265814f3ae597c6
NiiMon <35044329+NiiMon@users.noreply.github.com>
2018-07-31 22:51:55 -0700

    added several other approaches

--- path-sum-ii.java
+++ path-sum-ii.java
@@ -80,6 +80,7 @@ class Solution {
 
 
 // dfs top-down
+// tree height fits in cache, so call stack won't overflow
 class Solution {
     public List<List<Integer>> pathSum(TreeNode root, int sum) {
         List<List<Integer>> result = new ArrayList();
@@ -115,5 +116,174 @@ class Solution {
 }
 // 114 / 114 test cases passed.
 // Runtime: 2 ms
+
+
+// dfs top-down
+// tree height won't fit in cache but fits in memory
+class Solution {
+    class MyNode {
+        TreeNode _node;
+        int _height;
+        int _need;
+        public MyNode(TreeNode node, int height, int need) {
+            _node = node;
+            _height = height;
+            _need = need;
+        }
+    }
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        List<Integer> path = new ArrayList<>();
+        Stack<MyNode> stack = new Stack<>();
+        stack.push(new MyNode(root, 0, sum));
+        while (!stack.isEmpty()) {
+            MyNode node = stack.pop();
+            
+            // add to path
+            if (node._height == path.size()) {
+                path.add(node._node.val);
+            } else {
+                path.set(node._height, node._node.val);
+            }
+            
+            // op at leaf
+            if (node._node.left == null && node._node.right == null) {
+                if (node._need == node._node.val) {
+                    result.add(new ArrayList<>(path.subList(0, node._height + 1)));
+                }
+            }
+            
+            // generate children
+            if (node._node.left != null) {
+                stack.push(new MyNode(node._node.left, node._height + 1, node._need - node._node.val));
+            }
+            if (node._node.right != null) {
+                stack.push(new MyNode(node._node.right, node._height + 1, node._need - node._node.val));
+            }
+        }
+        
+        return result;
+    }
+}
+// 114 / 114 test cases passed.
+// Runtime: 6 ms
+
+
+// BFS
+class Solution {
+    class MyNode {
+        TreeNode _node;
+        List<Integer> _path;
+        int _need;
+        public MyNode(TreeNode node, List<Integer> path, int need) {
+            _node = node;
+            _path = path;
+            _need = need;
+        }
+    }
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        
+        Queue<MyNode> queue = new LinkedList<>();
+        queue.add(new MyNode(root, new ArrayList<>(), sum));
+        
+        while (!queue.isEmpty()) {
+            MyNode node = queue.remove();
+            
+            // op at node
+            node._path.add(node._node.val);
+            
+            // op at leaf node
+            if (node._node.left == null && node._node.right == null) {
+                if (node._node.val == node._need) {
+                    result.add(node._path);
+                }
+            }
+            
+            // generate children
+            if (node._node.left != null) {
+                queue.add(new MyNode(node._node.left, new ArrayList<>(node._path), node._need - node._node.val));
+            }
+            if (node._node.right != null) {
+                queue.add(new MyNode(node._node.right, new ArrayList<>(node._path), node._need - node._node.val));
+            }
+        }
+        return result;
+    }
+}
+// 114 / 114 test cases passed.
+// Runtime: 8 ms
+// every node saves root-to-node path, even if it won't be an answer
+// waste of space
+
+
+// BFS
+// for each node, remember its parent
+// when found an answer, walk backwards to generate root-to-node path
+class Solution {
+    class MyNode {
+        TreeNode _node;
+        MyNode _parent;
+        int _need;
+        public MyNode(TreeNode node, MyNode parent, int need) {
+            _node = node;
+            _parent = parent;
+            _need = need;
+        }
+    }
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        
+        Queue<MyNode> queue = new LinkedList<>();
+        queue.add(new MyNode(root, null, sum));
+        
+        while (!queue.isEmpty()) {
+            MyNode node = queue.remove();
+            
+            // op at node
+            // do nothing
+            
+            // op at leaf node
+            if (node._node.left == null && node._node.right == null) {
+                if (node._node.val == node._need) {
+                    result.add(walker(node));
+                }
+            }
+            
+            // generate children
+            if (node._node.left != null) {
+                queue.add(new MyNode(node._node.left, node, node._need - node._node.val));
+            }
+            if (node._node.right != null) {
+                queue.add(new MyNode(node._node.right, node, node._need - node._node.val));
+            }
+        }
+        return result;
+    }
+    private List<Integer> walker(MyNode end) {
+        List<Integer> result = new LinkedList<>();
+        if (end == null) {
+            return result;
+        }
+        
+        while (end != null) {
+            result.add(0, end._node.val);
+            end = end._parent;
+        }
+        
+        return result;
+    } 
+}
+// 114 / 114 test cases passed.
+// Runtime: 3 ms
 
 
