a09714e646af3ea2adace0d56fe3d057e5631003
NiiMon <35044329+NiiMon@users.noreply.github.com>
2018-08-31 17:29:48 -0700

    add union find solution

--- friend-circles.java
+++ friend-circles.java
@@ -40,6 +40,7 @@ If M[i][j] = 1, then M[j][i] = 1.
 
 */
 
+// Undirected Graph DFS
 class Solution {
     public int findCircleNum(int[][] M) {
         final int n = M.length;
@@ -71,3 +72,83 @@ class Solution {
 // 113 / 113 test cases passed.
 // Runtime: 6 ms
 
+
+
+// Union Find
+class Solution {
+    public int findCircleNum(int[][] M) {
+        if (M.length == 0 || M[0].length == 0) {
+            return 0;
+        }
+        
+        UnionFind uf = new UnionFind(M.length);
+        for (int i = 0; i < M.length; i++) {
+            for (int j = i + 1; j < M[0].length; j++) {
+                if (M[i][j] == 1) {
+                    uf.union(i, j);
+                }
+            }
+        }
+        
+        return uf._groups;
+    }
+    class UnionFind {
+        private final int _size;
+        private int[] _parent;
+        private int[] _rank;
+        private int _groups;
+        
+        public UnionFind(int size) {
+            _size = size;
+            _parent = new int[size];
+            _rank = new int[size];
+            for (int i = 0; i < size; i++) {
+                _parent[i] = i;
+                _rank[i] = 1;
+            }
+            _groups = size;
+        }
+        
+        public int find(int p) {
+            // find parent
+            int root = _parent[p];
+            while (_parent[root] != root) {
+                root = _parent[root];
+            }
+            // path compression
+            while (_parent[p] != root) {
+                int next = _parent[p];
+                _parent[p] = root;
+                p = next;
+            }
+            
+            return root;
+        }
+        
+        public boolean connected(int p, int q) {
+            return find(p) == find(q);
+        }
+        
+        public void union(int p, int q) {
+            if (connected(p, q)) {
+                return;
+            }
+            
+            int root1 = find(p);
+            int root2 = find(q);
+            
+            if (_rank[root1] > _rank[root2]) {
+                _parent[root2] = root1;
+            } else if (_rank[root1] < _rank[root2]) {
+                _parent[root1] = root2;
+            } else {
+                _parent[root2] = root1;
+                _rank[root1]++;
+            }
+            
+            _groups--;
+        }
+    }
+}
+// 113 / 113 test cases passed.
+// Runtime: 7 ms
