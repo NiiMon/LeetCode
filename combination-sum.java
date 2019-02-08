bcdb51333da9e775063aca9a2e8066ce01f5c0b7
NiiMon <35044329+NiiMon@users.noreply.github.com>
2019-02-07 23:50:11 -0800

    modify graph dfs approach

--- combination-sum.java
+++ combination-sum.java
@@ -34,117 +34,65 @@ A solution set is:
 */
 
 
-// DFS Top-Down
-class Solution {
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> results = new ArrayList<>();
-        if (candidates.length == 0) {
-            return results;
-        }
-
-        Stack<Integer> path = new Stack<>();
-        dfs(candidates.length - 1, target, candidates, path, results);
-
-        return results;
-    }
-
-    private void dfs(int index, int target, int[] candidates, Stack<Integer>
-    path, List<List<Integer>> results) {
-        
-        // op at node
-        if (index >= 0 && target == 0) {
-            results.add(new ArrayList(path));
-        }
-
-        // go down to children
-        // left child: use candidates[index] once
-        if (index >= 0 && target - candidates[index] >= 0) {
-            path.push(candidates[index]);
-            dfs(index, target - candidates[index], candidates, path, results);
-            path.pop();
-        }
-
-        // right child: discard candidates[index]
-        if (index >= 1 && target > 0) {
-            dfs(index - 1, target, candidates, path, results);
-        }
-    }
-}
-// 168 / 168 test cases passed.
-// Runtime: 13 ms
-
-
-
-// graph DFS Bottom-Up
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         if (candidates.length == 0) {
             return new ArrayList<>();
         }
+        
+        Arrays.sort(candidates);
 
-        Map<Key, List<List<Integer>>> saved = new HashMap<>();
-        return dfs(candidates.length - 1, target, candidates, saved);
+        return dfs(candidates, candidates.length - 1, target, new HashMap<>());
     }
-
-    private List<List<Integer>> dfs(int index, int target, int[] candidates,
-    Map<Key, List<List<Integer>>> saved) {
-    
-        Key node = new Key(index, target);
-        if (saved.containsKey(node)) {
-            return saved.get(node);
+    private List<List<Integer>> dfs(int[] candidates, int index, int val, Map<Key, List<List<Integer>>> saved) {
+        Key key = new Key(index, val);
+        if (saved.containsKey(key)) {
+            return saved.get(key);
         }
 
         List<List<Integer>> result = new ArrayList<>();
-
-        // base case: leaf with answer
-        if (node._index >= 0 && node._target == 0) {
-            List<Integer> list = new ArrayList<>();
-            result.add(list);
-        }
-
-        // general case: non-leaf node
-        if (node._index >= 0 && node._target > 0) {
-
-            List<List<Integer>> leftResult = dfs(node._index, node._target -
-            candidates[node._index], candidates, saved);
-            
-            List<List<Integer>> rightResult = dfs(node._index - 1, node._target,
-            candidates, saved);
-            
-            for (List<Integer> list : leftResult) {
-                List<Integer> copy = new ArrayList<>(list);
-                copy.add(candidates[node._index]);
+        
+        if (index < 0 || val < 0 || val != 0 && val < candidates[0]) {
+            // branch pruning: do nothing
+        } else if (val == 0) {
+            // base case: leaf node with answer
+            result.add(new ArrayList<>());
+        } else {
+            // general case: non-leaf node
+            // left: use current element once
+            for (List<Integer> sub : dfs(candidates, index, val - candidates[index], saved)) {
+                List<Integer> copy = new ArrayList<>(sub);
+                copy.add(candidates[index]);
                 result.add(copy);
             }
-            result.addAll(rightResult);
+
+            // right: skipping current element
+            result.addAll(dfs(candidates, index - 1, val, saved));
         }
 
-        saved.put(node, result);
+        saved.put(key, result);
+
         return result;
     }
-
     class Key {
         int _index;
-        int _target;
-        public Key(int index, int target) {
+        int _val;
+        public Key(int index, int val) {
             _index = index;
-            _target = target;
+            _val = val;
         }
-
-        @Override
         public int hashCode() {
-            return 31 * _target + _index;
+            return _index * 31 + _val;
         }
-
-        @Override
         public boolean equals(Object that) {
-            return that != null && that instanceof Key &&
+            return that instanceof Key &&
                     ((Key)that)._index == _index &&
-                    ((Key)that)._target == _target;
+                    ((Key)that)._val == _val;
         }
     }
 }
 // 168 / 168 test cases passed.
-// Runtime: 27 ms
-
+// Status: Accepted
+// Runtime: 17 ms
+// Memory Usage: 30.6 MB
 
